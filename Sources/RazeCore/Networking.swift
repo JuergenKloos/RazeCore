76cf6430f80f392a27b37331ca818da409b62162
Jürgen Kloos <juergen.kloos@gmx.net>
2020-06-18 19:54:47 +0800

    Added networking API for sending data

--- Sources/RazeCore/Networking.swift
+++ Sources/RazeCore/Networking.swift
@@ -9,6 +9,8 @@ import Foundation
 
 protocol NetworkSession {
     func get(from url: URL, completionHandler: @escaping (Data?, Error?) -> Void)
+    
+    func post(with request: URLRequest, completionHandler: @escaping (Data?, Error?) -> Void)
 }
 
 extension URLSession : NetworkSession {
@@ -19,7 +21,13 @@ extension URLSession : NetworkSession {
         task.resume()
     }
     
-
+    func post(with request: URLRequest, completionHandler: @escaping (Data?, Error?) -> Void) {
+        let task = dataTask(with: request) { data, _, error in
+            completionHandler(data, error)
+        }
+        task.resume()
+    }
+    
 }
 
 extension RazeCore {
@@ -40,6 +48,26 @@ extension RazeCore {
                 session.get(from: url) { data, error in
                     let result = data.map(NetworkResult<Data>.success) ?? .failure(error)
                     completionHandler(result)
+                }
+            }
+            
+            /// Calls to the live Internet to post Data to a specific location
+            /// - Parameters:
+            ///   - url: The location you wish to post data to
+            ///   - body: The data to be posted
+            ///   - completionHandler: Returns a result object which signifies the status of the request
+            public func sendData<I : Codable>(to url: URL, body: I, completionHandler: @escaping (NetworkResult<Data>) -> Void) {
+                var request = URLRequest(url: url)
+                do {
+                    let httpBody = try JSONEncoder().encode(body)
+                    request.httpBody = httpBody
+                    request.httpMethod = "POST"
+                    session.post(with: request) { data, error in
+                        let result = data.map(NetworkResult<Data>.success) ?? .failure(error)
+                        completionHandler(result)
+                    }
+                } catch let error {
+                    return completionHandler(.failure(error))
                 }
             }
             
--- Tests/RazeCoreTests/RazeNetworkingTests.swift
+++ Tests/RazeCoreTests/RazeNetworkingTests.swift
@@ -19,6 +19,14 @@ final class RazeNetworkingTests: XCTestCase {
             completionHandler(data, error)
         }
         
+        func post(with request : URLRequest, completionHandler: @escaping (Data?, Error?) -> Void) {
+            completionHandler(data, error)
+        }
+    }
+    
+    struct MockData : Codable, Equatable {
+        var id : Int
+        var name : String
         
     }
     
@@ -44,11 +52,38 @@ final class RazeNetworkingTests: XCTestCase {
         wait(for: [expectation], timeout: 5)
     }
     
-    
+    func testSendDataCall() {
+        let session = NetworkSessionMock()
+        let manager = RazeCore.Networking.Manager()
+        
+        let sampleObject = MockData(id: 1, name: "David")
+        let data = try? JSONEncoder().encode(sampleObject)
+        
+        manager.session = session
+        session.data = data
+        
+        let url = URL(fileURLWithPath: "url")
+        let expectation = XCTestExpectation(description: "sent data")
+        
+        manager.sendData(to: url, body: sampleObject) { result in
+            expectation.fulfill()
+            switch result {
+            case .success(let returnedData):
+                let returnedObject = try? JSONDecoder().decode(MockData.self, from: returnedData)
+                XCTAssertEqual(sampleObject, returnedObject)
+            case .failure(let error):
+                XCTFail(error?.localizedDescription ?? "error forming error result")
+            }
+        }
+        
+        wait(for: [expectation], timeout: 5)
+        
+    }
 
     
     static var allTests = [
-        ("testLoadDataCall", testLoadDataCall)
+        ("testLoadDataCall", testLoadDataCall),
+        ("testSendDataCall", testSendDataCall)
     ]
     
  
