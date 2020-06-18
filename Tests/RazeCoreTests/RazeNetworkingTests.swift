c8559ca1ac12190bd4d32245214b460d4ce0b49b
Jürgen Kloos <juergen.kloos@gmx.net>
2020-06-18 17:13:52 +0800

    Refactored networking API to allow dependency injection for easier testing

--- Tests/RazeCoreTests/RazeNetworkingTests.swift
+++ Tests/RazeCoreTests/RazeNetworkingTests.swift
@@ -10,17 +10,33 @@ import XCTest
 
 final class RazeNetworkingTests: XCTestCase {
 
+    class NetworkSessionMock : NetworkSession {
+        
+        var data : Data?
+        var error : Error?
+        
+        func get(from url: URL, completionHandler: @escaping (Data?, Error?) -> Void) {
+            completionHandler(data, error)
+        }
+        
+        
+    }
+    
     func testLoadDataCall() {
         let manager = RazeCore.Networking.Manager()
+        let session = NetworkSessionMock()
+        manager.session = session
         let expectation = XCTestExpectation(description: "Called for data")
-        guard let url = URL(string: "https://raywenderlich.com") else {
-            return XCTFail("Could not create URL")
-        }
+
+        let data = Data([0, 1, 0, 1])
+        session.data = data
+        let url = URL(fileURLWithPath: "url")
+        
         manager.loadData(from: url) { result in
             expectation.fulfill()
             switch result {
             case .success(let returnedData):
-                XCTAssertNotNil(returnedData, "Response data is nil")
+                XCTAssertEqual(data, returnedData, "manager returned unexpected data")
             case .failure(let error):
                 XCTFail(error?.localizedDescription ?? "error forming error result")
             }
